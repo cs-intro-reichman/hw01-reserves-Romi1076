f211525edff9d0b1c443bfb2e68e1811cef97c79
Your Name <Romitiffany.mechalo@runi.ac.il>
2024-01-25 19:41:11 +0200

    Add awesome code for Homework 1

--- AddTwo.java
+++ AddTwo.java
@@ -1,12 +1,10 @@
-/*
- * Adds two given integers and prints the result in a fancy way.
- */
+
 public class AddTwo {
 	public static void main(String[] args) {
-	int first = Integer.parseInt(args[0]);
-	int second = Integer.parseInt(args[1]);
-	
-	System.out.println(first);
-	System.out.println(second);
+	int a = Integer.parseInt(args[0]);
+	int b = Integer.parseInt(args[1]);
+		int sum = a + b;
+		System.out.println(a + " + " + b + " = " + sum);	
 	}
+	
 }
--- GenThree.java
+++ GenThree.java
@@ -1,7 +1,4 @@
-/*
- * Generates three random integers, each in a given range [a,b),
- * prints them, and then prints the minimal number that was generated.
- */
+
 public class GenThree {
 	public static void main(String[] args) {
 		
