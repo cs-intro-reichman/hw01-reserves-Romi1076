aeda872fc57848e74b708a74458ee0767f5ae6f6
Your Name <Romitiffany.mechalo@runi.ac.il>
2024-01-25 19:59:24 +0200

    Add awesome code for Homework 1

--- Coins.java
+++ Coins.java
@@ -1,12 +1,11 @@
 
 public class Coins {
 	public static void main(String[] args) {
-		int numcents = Integer.parseInt(args[0]);
-		int quarters = numcents / 25;
-        int remainderCents = numcents % 25;
+	    
+        int totalnumCents = Integer.parseInt(args[0]);
+        int quarters = totalnumCents / 25; 
+        int remainderCents = totalnumCents % 25; 
 
-	System.out.println("  please use " + quarters +  " quarters and " + remainderCents  + " cents " );
-
+        System.out.println("Use " + quarters + " quarters and " + remainderCents + " cents");
 	}
-
 }
